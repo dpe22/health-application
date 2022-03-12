6d7c0fd7a0398932d4dad98b4d6ebf8763034a7e
dpe22 <dpe22@bu.edu>
2022-03-12 18:55:43 -0500

    modularized login system

---
+++ login_system.py
@@ -0,0 +1,33 @@
+import hashlib
+
+def signup():
+    
+    email = input("Enter email address: ")
+    pwd = input("Enter password: ")
+    conf_pwd = input("Confirm password: ")
+    
+    if conf_pwd == pwd:
+        enc = conf_pwd.encode()
+        hash1 = hashlib.md5(enc).hexdigest()
+        with open("credentials.txt", "w") as f:
+            f.write(email + "\n")
+            f.write(hash1)
+        f.close()
+        print("You have registered successfully!")
+        return
+
+    else:
+        print("Password is not same as above! \n")
+
+def login():
+    email = input("Enter email: ")
+    pwd = input("Enter password: ")
+    auth = pwd.encode()
+    auth_hash = hashlib.md5(auth).hexdigest()
+    with open("credentials.txt", "r") as f:
+        stored_email, stored_pwd = f.read().split("\n")
+    f.close()
+    if email == stored_email and auth_hash == stored_pwd:
+        print("Logged in Successfully!")
+    else:
+         print("Login failed! \n")
