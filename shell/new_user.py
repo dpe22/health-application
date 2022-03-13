3535e4b0b802de207299f3f38c4de34cb716bcb7
dpe22 <dpe22@bu.edu>
2022-03-12 21:43:15 -0500

    update new_user

--- shell/new_user.py
+++ shell/new_user.py
@@ -1,14 +1,13 @@
-
 import npyscreen
 
 class NewUser(npyscreen.NPSApp):
-    def main(self):
+    def add_user(self):
         # These lines create the form and populate it with widgets.
         # A fairly complex screen in only 8 or so lines of code - a line for each control.
         F  = npyscreen.Form(name = "Add a new user to Health App",)
-        t  = F.add(npyscreen.TitleText, name = "First Name:",)
-        fn = F.add(npyscreen.TitleText, name = "Last Name:",)
-        fn2 = F.add(npyscreen.TitleText, name="Email:")
+        fn  = F.add(npyscreen.TitleText, name = "First Name:",)
+        ln = F.add(npyscreen.TitleText, name = "Last Name:",)
+        em = F.add(npyscreen.TitleText, name="Email:")
         dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
         ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, name="Select role(s) using 'x':",
                 values = ["Patient","Relative","Practitioner","Administrator","Device Developer"], scroll_exit=True)
@@ -18,50 +17,7 @@ class NewUser(npyscreen.NPSApp):
 
         # print(ms2.get_selected_objects())
         print("Thank you!")
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print()
-        print("Have a Great Day!")
+        return fn,ln,em,dt,ms2
 
 if __name__ == "__main__":
     App = NewUser()
